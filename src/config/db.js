16e484b3b75850a1e49ed0be99ac049d15582367
valzara.official <valzara.official@gmail.com>
2026-09-13 16:02:25 +0700

    cập nhật v2

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_URI || 'mongodb://localhost:27017/my-project-db', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.DB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
